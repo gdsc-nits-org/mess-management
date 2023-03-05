bf181d324199cf10617732e64f0938be4468c317
Jugya K. Gogoi <crjugya123@gmail.com>
2023-03-05 18:41:48 +0530

    fix: errors in noticeboard component

--- src/Components/NoticeBoardCard/Card.jsx
+++ src/Components/NoticeBoardCard/Card.jsx
@@ -1,20 +1,16 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/jsx-no-comment-textnodes */
+import { Icon } from "@iconify/react";
 import { useState } from "react";
 import { formatDistanceToNow } from "date-fns";
 import Button from "../Button/Button";
 import styles from "./Card.module.scss";
-import open from "../../assets/bookmark(open).svg";
-import closed from "../../assets/bookmark(closed).svg";
 import NoticeModal from "./NoticeModal";
+
 const Card = ({ text, date }) => {
   const [active, setActive] = useState(false);
   const [modal, setModal] = useState(false);
+
   const handleClick = () => {
-    setActive((prev) => {
-      return !prev;
-    });
+    setActive(!active);
   };
   const handleOpen = () => {
     setModal((prev) => {
@@ -24,7 +20,15 @@ const Card = ({ text, date }) => {
   return (
     <div className={styles.card}>
       <div className={styles.img}>
-        <img src={active ? closed : open} alt="on" onClick={handleClick} role="button" />
+        <Icon
+          icon={
+            active ? "material-symbols:bookmark-outline" : "material-symbols:bookmark"
+          }
+          width="19"
+          height="25"
+          color="#7293bc"
+          onClick={handleClick}
+        />
       </div>
       <div className={styles.text}>{text}</div>
       <div className={styles.date}>
